a546243a09ac07de7dfcb6913ae22c9a2a679849
Quang Dao <quang1425valentine@gmail.com>
2023-07-06 11:16:55 +0700

    add Sign up page

---
+++ src/views/SignupView.vue
@@ -0,0 +1,141 @@
+<template>
+  <a-form :form="form" @submit="handleSubmit" style="padding: 50px">
+    <a-form-item v-bind="formItemLayout" label="Full name">
+      <a-input
+        v-decorator="[
+          'fullname',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Please input your Full name!'
+              }
+            ]
+          }
+        ]"
+      />
+    </a-form-item>
+    <a-form-item v-bind="formItemLayout" label="Username">
+      <a-input
+        v-decorator="[
+          'username',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Please input your Username!'
+              }
+            ]
+          }
+        ]"
+      />
+    </a-form-item>
+    <a-form-item v-bind="formItemLayout" label="Password" has-feedback>
+      <a-input
+        v-decorator="[
+          'password',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Please input your password!'
+              },
+              {
+                validator: validateToNextPassword
+              }
+            ]
+          }
+        ]"
+        type="password"
+      />
+    </a-form-item>
+    <a-form-item v-bind="formItemLayout" label="Confirm Password" has-feedback>
+      <a-input
+        v-decorator="[
+          'confirm',
+          {
+            rules: [
+              {
+                required: true,
+                message: 'Please confirm your password!'
+              },
+              {
+                validator: compareToFirstPassword
+              }
+            ]
+          }
+        ]"
+        type="password"
+        @blur="handleConfirmBlur"
+      />
+    </a-form-item>
+    <a-form-item v-bind="tailFormItemLayout">
+      <a-button type="primary" html-type="submit"> Register </a-button>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      confirmDirty: false,
+      autoCompleteResult: [],
+      formItemLayout: {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 }
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 }
+        }
+      },
+      tailFormItemLayout: {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0
+          },
+          sm: {
+            span: 16,
+            offset: 8
+          }
+        }
+      }
+    };
+  },
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "register" });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    },
+    handleConfirmBlur(e) {
+      const value = e.target.value;
+      this.confirmDirty = this.confirmDirty || !!value;
+    },
+    compareToFirstPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && value !== form.getFieldValue("password")) {
+        callback("Two passwords that you enter is inconsistent!");
+      } else {
+        callback();
+      }
+    },
+    validateToNextPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && this.confirmDirty) {
+        form.validateFields(["confirm"], { force: true });
+      }
+      callback();
+    }
+  }
+};
+</script>
