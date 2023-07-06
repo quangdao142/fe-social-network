cc2da24300d1f714a8b98101c77e1799214558c4
Quang Dao <quang1425valentine@gmail.com>
2023-07-06 11:15:56 +0700

    edit Home Content

--- src/views/HomeContent.vue
+++ src/views/HomeContent.vue
@@ -3,7 +3,6 @@
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '100vh' }">
       <a-list :grid="{ gutter: 16, column: 4 }" :data-source="data">
         <a-list-item slot="renderItem" slot-scope="item">
-
           <a-card hoverable style="width: 300px">
             <img slot="cover" alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />
             <template slot="actions">
@@ -14,12 +13,13 @@
               <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             </a-card-meta>
             <div>
-              <p style="padding: 15px 0 0 5px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quae voluptatem hic excepturi.</p>
+              <p style="padding: 15px 0 0 5px">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quae voluptatem hic excepturi.
+              </p>
               <a-tag>Tag 1</a-tag>
               <a-tag>Tag 2</a-tag>
             </div>
           </a-card>
-
         </a-list-item>
       </a-list>
     </a-layout-content>
